18f6459cb72ce98cf37d65f117c532396c28ed28
SandroCristiano <sandro@darklife.org>
2020-09-22 19:34:13 -0300

    Commit of the project for the Getting and Cleaning Data Course Project.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Darklife
-This is my first repo. As I advance through my Data Science course.
+
+Peer-graded Assignment: Getting and Cleaning Data Course Project
+
+This repository contains the submission by Sandro de Castro for the Getting and Cleaning Data Course Project from Johns Hopkins via Coursera. It contains the instructions on how to run analysis on the Human Activity recognition data set.
+
+Data set
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Files
+
+CodeBook.md - this book describes the variables, the data, and any transformations or work that have been performed to clean up the data.
+
+run_analysis.R - this script performs the data preparations and the the 5 required steps described in the course project's definition: 
+
+  - Merges the training and the test sets to create one data set.
+  - Extracts only the measurements on the mean and standard deviation for each measurement.
+  - Uses descriptive activity names to name the activities in the data set
+  - Appropriately labels the data set with descriptive variable names.
+  - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+  
+  FinalData.txt - the exported final data after following the 5 required steps listed above. 
---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+# Step 1: This script merges the training and test sets to create one data frame. 
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+#Step 2: Extracting only the measurements on the mean and standard deviations of each measuments.
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+#Step 3: Using descriptive activity names in order to name the activities in the data set.
+TidyData$code <- activities[TidyData$code, 2]
+
+# Step 4: Giving proper labels with descriptive variable names.
+names(TidyData)[2] = "activity"
+names(TidyData) <- gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData) <- gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData) <- gsub("BodyBody", "Body", names(TidyData))
+names(TidyData) <- gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData) <- gsub("^t", "Time", names(TidyData))
+names(TidyData) <- gsub("^f", "Frequency", names(TidyData))
+names(TidyData) <- gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData) <- gsub("-mean", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-std", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("-freq", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData) <- gsub("angle", "Angle", names(TidyData))
+names(TidyData) <- gsub("gravity", "Gravity", names(TidyData))
+
+# Step 5: Creates a second, independent tidy data set  with the average of each variable for each activity and each subject.
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarize_all(list(mean))
+write.table(FinalData, "FinalData.txt", row.name = FALSE)
